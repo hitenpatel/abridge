e72931532615b13f34496f1be70fbfadaf1ed3c0
Hiten Patel <hitenpatel2010@gmail.com>
2026-02-03 17:08:46 +0000

    feat(api): register stripe router

--- apps/api/src/router/index.ts
+++ apps/api/src/router/index.ts
@@ -2,12 +2,14 @@ import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { healthRouter } from "./health";
 import { messagingRouter } from "./messaging";
+import { stripeRouter } from "./stripe";
 import { userRouter } from "./user";
 
 export const appRouter = router({
 	health: healthRouter,
 	auth: authRouter,
 	messaging: messagingRouter,
+	stripe: stripeRouter,
 	user: userRouter,
 });
 
