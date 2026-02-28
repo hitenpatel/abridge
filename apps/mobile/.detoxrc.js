de55b8bfa87b6c5253dae445081b9d19036489f0
Hiten Patel <hitenpatel2010@gmail.com>
2026-02-28 17:47:06 +0000

    style: fix Biome formatting in Detox config files

--- apps/mobile/.detoxrc.js
+++ apps/mobile/.detoxrc.js
@@ -14,8 +14,7 @@ module.exports = {
 		"android.debug": {
 			type: "android.apk",
 			binaryPath: "android/app/build/outputs/apk/debug/app-debug.apk",
-			testBinaryPath:
-				"android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk",
+			testBinaryPath: "android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk",
 			build:
 				"cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..",
 			reversePorts: [8081],
