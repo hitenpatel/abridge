da819a31564c2294412765f4c1abc2164982972a
Hiten Patel <hitenpatel2010@gmail.com>
2026-03-06 07:46:20 +0000

    ci: disable reanimated auto-detection in babel-preset-expo
    Set reanimated: false to prevent babel-preset-expo from auto-detecting
    and loading react-native-worklets/plugin. The reanimated plugin is
    already explicitly configured in the plugins array.

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = (api) => {
 	api.cache(true);
 	return {
-		presets: [["babel-preset-expo", { jsxImportSource: "nativewind", worklets: false }], "nativewind/babel"],
+		presets: [["babel-preset-expo", { jsxImportSource: "nativewind", reanimated: false }], "nativewind/babel"],
 		plugins: ["react-native-reanimated/plugin"],
 	};
 };
